15f7d3930aef885248030786f05aa820e509a6c7
Dave Bouwman <dave@davebouwman.com>
2018-11-09 10:05:20 -0700

    upgrade to 3.5, all tests passing

--- tests/unit/components/json-editor-test.js
+++ tests/unit/components/json-editor-test.js
@@ -1,27 +1,24 @@
-import { moduleForComponent, test } from 'ember-qunit';
+import { module, test } from 'qunit';
+import { setupTest } from 'ember-qunit';
 
-moduleForComponent('json-editor', 'Unit | Component | json editor', {
-  // Specify the other units that are required for this test
-  // needs: ['component:foo', 'helper:bar'],
-  unit: true
-});
+module('Unit | Component | json editor', function(hooks) {
+  setupTest(hooks);
 
-test('disabled', function(assert) {
+  test('disabled', function(assert) {
 
-  const component = this.subject();
+    const component = this.owner.factoryFor('component:json-editor').create();
 
-  this.render();
+    let options = component.get('options');
 
-  let options = component.get('options');
+    assert.equal(component.get('disabled'), false);
+    assert.equal(options.mode, 'tree');
+    assert.deepEqual(options.modes.sort(), ['tree', 'view', 'form', 'text', 'code'].sort());
 
-  assert.equal(component.get('disabled'), false);
-  assert.equal(options.mode, 'tree');
-  assert.deepEqual(options.modes.sort(), ['tree', 'view', 'form', 'text', 'code'].sort());
+    component.set('disabled', true);
+    options = component.get('options');
 
-  component.set('disabled', true);
-  options = component.get('options');
-
-  assert.equal(component.get('disabled'), true);
-  assert.equal(options.mode, 'view');
-  assert.deepEqual(options.modes, ['view']);
+    assert.equal(component.get('disabled'), true);
+    assert.equal(options.mode, 'view');
+    assert.deepEqual(options.modes, ['view']);
+  });
 });
